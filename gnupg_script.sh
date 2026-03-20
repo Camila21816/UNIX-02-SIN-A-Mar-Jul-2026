ac494480f8158b0bd58f0fc420a8c7475fcd05c5
Camila21816 <capaucargu@uide.edu.ec>
2026-03-20 17:36:08 +0000

    gpg --version

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,2 +1,3 @@
 uname -a #daba información del ordenador
-which gpg #donde esta el binario de gpg
+which gpg #donde esta ubicado el binario de gpg
+gpg --version #muestra la versión e información adicional
