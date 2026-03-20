43dbf99c7e80d8409f317849ff36bf31e8e1c826
Camila21816 <capaucargu@uide.edu.ec>
2026-03-20 18:02:24 +0000

    list and key generation

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,4 +1,4 @@
 uname -a #daba información del ordenador
 which gpg #donde esta ubicado el binario de gpg
 gpg --version #muestra la versión e información adicional
-gpg --full-generate-key #genera llave publica y privada
+gpg --full-generate-key #genera llave publica y privada.
