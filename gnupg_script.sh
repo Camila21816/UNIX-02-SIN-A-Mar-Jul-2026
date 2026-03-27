91b8c4348816b0339f7da201b89f31e1c57a6656
Camila21816 <capaucargu@uide.edu.ec>
2026-03-27 22:57:35 +0000

    gpg --list-secret-keys --keyid-format=long list private keys

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -3,3 +3,4 @@ which gpg #donde esta ubicado el binario de gpg
 gpg --version #muestra la versión e información adicional
 gpg --full-generate-key #genera llave publica y privada.
 gpg --armor --export #exportaba la llave publica
+gpg --list-secret-keys --keyid-format=long # Sirve para listar las llaves privadas
