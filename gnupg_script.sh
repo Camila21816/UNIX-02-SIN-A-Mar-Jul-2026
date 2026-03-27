708683d57591a61517549c662626112aebeedb08
Camila21816 <capaucargu@uide.edu.ec>
2026-03-27 23:38:42 +0000

    gpg --list-keys Verify the import

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,4 +5,5 @@ gpg --full-generate-key #genera llave publica y privada.
 gpg --armor --export #exportaba la llave publica
 gpg --list-secret-keys --keyid-format=long # Sirve para listar las llaves privadas
 gpg --armor --export-secret-keys #sirve para exportar la llave privada
-gpg --import #aqui se pone el nombre del arcgivo donde hayamos guardado la llave publica de nuestro compañero y este comando importara la llave publica.
+gpg --import #aqui se pone el nombre del arcgivo donde hayamos guardado la llave publica de nuestro compañero y este comando importara la llave publica
+gpg --list-keys #verifica que la importación de la llave haya sido correcta
