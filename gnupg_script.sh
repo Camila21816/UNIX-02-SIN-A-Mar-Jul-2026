fcbc4abe0cf7111b8081c3342dabbf51a711e913
Camila21816 <capaucargu@uide.edu.ec>
2026-03-28 02:17:40 +0000

    pg --decrypt open encrypted file

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,15 +1,20 @@
+#bloque A preparacion del entorno
 uname -a #daba información del ordenador
 which gpg #donde esta ubicado el binario de gpg
 gpg --version #muestra la versión e información adicional
 gpg --full-generate-key #genera llave publica y privada.
 gpg --armor --export #exportaba la llave publica
 gpg --list-secret-keys --keyid-format=long # Sirve para listar las llaves privadas
+#bloque B - intercambio de llaves publicas
 gpg --armor --export-secret-keys #sirve para exportar la llave privada
 gpg --import #aqui se pone el nombre del arcgivo donde hayamos guardado la llave publica de nuestro compañero y este comando importara la llave publica
 gpg --list-keys #verifica que la importación de la llave haya sido correcta
+#bloque C - cifrado y descrifrado
 echo "Mensaje escrito desde mi cuenta principal" > doc_no_cifrado.txt #creacion del mensaje
 cat doc_no_cifrado.txt #muestra si se creo correctamente el documento 
 gpg --output doc_cifrado.txt --encrypt --recipient gogosaturo57@gmail.com doc_no_cifrado.txt #toma un archivo normal y lo convierte en un archivo secreto que solo el dueño de la llave puede abrir
 gpg --decrypt doc_cifrado.txt #va a desencriptar el mensaje 
+#bloque D - firma de documentos
 gpg --clearsign #firmar el contenido sin ocultar el texto original
 gpg --sign doc_no_cifrado.txt # Firmar en formato binario
+gpg --decrypt "doc_no_cifradosecundario.txt (1).gpg" #abrir el archivo firmado con la contraseña
