cd3559a4196867e8fdc9f6e3022c2a04a19bffbe
Camila21816 <capaucargu@uide.edu.ec>
2026-04-02 12:48:36 +0000

    sudo apt update, upgrade, intall parted install parted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -1,2 +1,7 @@
 cd / #We move to the root directory
 ls -F #It is used to list the contents of the current directory
+#FHS estructura de archivos derivada de linux (/bin,/etc,/proc)
+#primera fase carga del FW, bootlader, kernel
+sudo apt update #update url list
+sudo apt upgrade #Install the latest versions of the libraries
+sudo apt install parted #install the software
