cfc7f939a00a76cb1701b77e53fd419bd79787e5
Camila21816 <capaucargu@uide.edu.ec>
2026-04-09 13:57:02 +0000

    kernel linux

---
+++ scipt_distro.sh
@@ -0,0 +1,8 @@
+sudo apt install -y git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux dosfstools qemu-system-x86
+#Install all necessary libraries
+git clone --depth 1 https://github.com/torvalds/linux.git cd linux
+#clone the kernel repository
+cd linux #I move to the Linux folder
+make menuconfig #Configure the compilation options
+
+make -j 2 # compile the kernel with the number of available cores
