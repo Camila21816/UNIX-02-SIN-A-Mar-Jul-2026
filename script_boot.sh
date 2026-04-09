fc23573fa434f60bcbacd3ce884bcdbb829b358f
Camila21816 <capaucargu@uide.edu.ec>
2026-04-09 12:31:12 +0000

    commands to move

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,6 @@
 mkdir proyecto #create the project directory
 cd proyecto/ #We move to the project directory
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto #I move to the project directory from the root directory
+pwd #It tells me which directory I'm in.
+cd ~ #It moves me to the home directory
+cd /home/codespace #absolute path
