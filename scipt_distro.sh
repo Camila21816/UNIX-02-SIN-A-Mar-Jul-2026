b28c1768eb14b10066136a1ae56cfbf2c534dfb3
Camila21816 <capaucargu@uide.edu.ec>
2026-04-10 17:35:36 +0000

    cd /boot-files/initramfs Enter the menu with the facilities

--- scipt_distro.sh
+++ scipt_distro.sh
@@ -12,3 +12,4 @@ git clone --depth 1 https://git.busybox.net/busybox #clone a git repository
 cd busybox #enter the busybox folder
 sudo mkdir /boot-files/initramfs #creates a specific folder structure in the system root
 sudo make CONFIG_PREFIX=/boot-files/initramfs install #Install the files that were compiled into the folder we prepared
+cd /boot-files/initramfs #Enter the busybox folder where all the files were installed
