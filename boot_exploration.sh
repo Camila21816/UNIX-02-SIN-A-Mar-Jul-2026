4f2603711bd21e6cd6946361156c39b6f436ca5d
Camila21816 <capaucargu@uide.edu.ec>
2026-04-16 13:09:16 +0000

    ls --help man ls man git-clone # help commands

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -1,4 +1,9 @@
 ls -a ls--all #they do the same
 ls -l -a -a ls -lah #all the options together in a single command
-mkdir -- -rf #create the directory -rf
+mkdir -- -rf #create the directory -rf #todo lo que viene despuesde -- se interpreta como texto
 rmdir -- -rf #remove -rf
+ls --help #short help
+man ls #complete help manual #to search I occupy / to go forward n and backward N
+man git #Table of Contents
+man git-clone #The clone command is usually your first step
+--depth #Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the histories near the tips of all branches. If you want to clone submodules shallowly, also pass --shallow-submodules.
