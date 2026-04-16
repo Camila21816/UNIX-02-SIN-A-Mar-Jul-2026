359d241a215fe744017f9d82daf26b2fdb63a1dc
Camila21816 <capaucargu@uide.edu.ec>
2026-04-16 13:47:52 +0000

    sudo commands and their functions

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -13,3 +13,10 @@ chmod +x script.sh #It turns the file into an executable for everyone (owner, gr
 chmod u+x script.sh #Grants execution permission only to the file owner, maintaining the restriction for all other system users.
 chmod o-r secreto.text #It blocks read access to external users (others), preventing people outside the owner or group from seeing the file's contents.
 chmod u+rw,go-rwx privado #It establishes full access control where the owner can read and edit, while the group and others lose any right to view, touch, or execute the file.
+sudo chmod +x init #Grants execute permissions to the file named init using superuser privileges. When using sudo, the system prompts you for your password to ensure you have the authority to modify protected or system files, and the +x (since there are no preceding letters) makes the file executable for all users.
+#init contiene el primer proseso de ejecucion 
+tee #It's a "pipe" with a standard output and a file
+sudo echo "hola">/etc/archivo_protegido #no working
+sudo echo "hola" | sudo tee /etc/archivo_protegido > /dev/null #This command is used to write text to a system-protected file without the terminal displaying unnecessary messages. `sudo tee` is used because it grants the process administrator privileges to save the content, while the final part (`> /dev/null`) clears the screen so you don't see repeated text while the file is being saved.
+ls /etc #List the contents of the /etc directory
+sudo echo "hola" | sudo tee /etc/archivo_protegido #It works but it doesn't redirect
