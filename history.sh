7fdcbab5217ddb2f6153940ba2533c740bba046b
Camila21816 <capaucargu@uide.edu.ec>
2026-04-24 16:45:08 +0000

    Fix problema 4: permisos simbolicos corregidos

--- history.sh
+++ history.sh
@@ -87,4 +87,45 @@ git commit -m "Fix problema 2: archivos movidos y renombrados"
    20  git commit -m "Fix problema 2: archivos movidos y renombrados"
 
    #problem 3
-   
+   @Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026 (eval_p2_1_g2) $ cd evaluation
+@Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/evaluation (eval_p2_1_g2) $ chmod 600 kepler/registros/sensores.log
+@Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/evaluation (eval_p2_1_g2) $ ls -l kepler/registros/sensores.log
+-rw------- 1 codespace codespace 270 Apr 24 16:16 kepler/registros/sensores.log
+@Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/evaluation (eval_p2_1_g2) $ cd ..cd ..
+bash: cd: too many arguments
+@Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/evaluation (eval_p2_1_g2) $ cd ..
+@Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026 (eval_p2_1_g2) $ git add .
+git commit -m "Fix problema 3: permisos numericos 600 en sensores.log"
+[eval_p2_1_g2 b3479e7] Fix problema 3: permisos numericos 600 en sensores.log
+ 1 file changed, 44 insertions(+)
+@Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026 (eval_p2_1_g2) $ history
+    1  git fetch upstream
+    2  git checkout -b eval_p2_1_g2 upstream/eval_p2_1_g2
+    3  git push origin eval_p2_1_g2
+    4  mv kepler/docs kepler/registros
+    5  ls -R
+    6  cd evaluation
+    7  mv kepler/docs kepler/registros
+    8  mkdir kepler/plantillas
+    9  history
+   10  cd ..
+   11  git add .
+   12  git commit -m "Fix problema 1: estructura de directorios corregida"
+   13  git log --oneline
+   14  cd evaluation
+   15  mv kepler/sensores.log kepler/registros/
+   16  mv kepler/ajustes.old kepler/ajustes.conf
+   17  ls -R kepler/
+   18  cd ..
+   19  git add .
+   20  git commit -m "Fix problema 2: archivos movidos y renombrados"
+   21  history
+   22  cd evaluation
+   23  chmod 600 kepler/registros/sensores.log
+   24  ls -l kepler/registros/sensores.log
+   25  cd ..cd ..
+   26  cd ..
+   27  git add .
+   28  git commit -m "Fix problema 3: permisos numericos 600 en sensores.log"
+
+#problem 4
