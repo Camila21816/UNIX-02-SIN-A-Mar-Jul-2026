337d690c382ed2318e5e425c7c4643c5f935abd8
Camila21816 <capaucargu@uide.edu.ec>
2026-04-24 16:38:12 +0000

    Fix problema 2: archivos movidos y renombrados

--- history.sh
+++ history.sh
@@ -28,3 +28,19 @@ README.txt
     6  cd evaluation
     7  mv kepler/docs kepler/registros
     8  mkdir kepler/plantilla
+    @Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/evaluation (eval_p2_1_g2) $ cd ..
+@Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026 (eval_p2_1_g2) $ git add .
+@Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026 (eval_p2_1_g2) $ git commit -m "Fix problema 1: estructura de directorios corregida"
+[eval_p2_1_g2 0ac4541] Fix problema 1: estructura de directorios corregida
+ 2 files changed, 30 insertions(+)
+ rename evaluation/kepler/{docs => registros}/README.txt (100%)
+ create mode 100644 history.sh
+@Camila21816 ➜ /workspaces/UNIX-02-SIN-A-Mar-Jul-2026 (eval_p2_1_g2) $ git log --oneline
+0ac4541 (HEAD -> eval_p2_1_g2) Fix problema 1: estructura de directorios corregida
+4e8a61c (upstream/eval_p2_1_g2, origin/eval_p2_1_g2) init
+f833e71 (upstream/main, upstream/HEAD) wip: devices as files
+a1be3af libs and key generation
+5be6c85 version
+bc53f37 which
+a10e55a uname
+0318299 Initial commit
