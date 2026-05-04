9b10f279158c8d3ae05fc8183ecd9486380ae619
Camila21816 <capaucargu@uide.edu.ec>
2026-05-04 04:22:24 +0000

    cd ~ go to personal directory

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -18,3 +18,4 @@ cd / #Go to the root directory
 cd /home/sysadmin #Change the current working directory to the home folder of the user "sysadmin".
 cd School/Art #It's a way to navigate through the file hierarchy to specifically locate the directory dedicated to your art content.
 cd .. #This command allows you to go back one level in the folder hierarchy. Instead of entering a new location, you "go up" to the folder that contains the one you are currently in.
+cd ~ #Upon execution, the system instantly transports us to the home directory.
