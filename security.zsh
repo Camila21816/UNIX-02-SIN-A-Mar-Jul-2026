7a03604292f4e97ddc61f3985779780a49fb5ec9
Camila21816 <capaucargu@uide.edu.ec>
2026-05-12 17:28:38 +0000

    collaboration between users through the use of groups.

--- security.zsh
+++ security.zsh
@@ -26,3 +26,9 @@ ls -l mi_archivo #It displays detailed information about the newly created file.
 useradd -m -s /usr/bin/zsh luna #Create the user named luna. It automatically creates the user's home directory, usually in /home/luna.
 chown luna mi_archivo #This command changes the owner of the previous file.
 #If we are not logged in as root, we must use sudo
+
+groups #This command shows which groups the current user belongs to.
+groupadd grupo_test #Create a new group in the system called group_test.
+touch comun #You create a file called common and check its details. 
+ ls -l comun #You check their details.
+ cat /etc/group #This command prints the contents of the configuration file where all system groups are stored.
