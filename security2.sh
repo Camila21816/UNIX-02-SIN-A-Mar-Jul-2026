f637526739dbf89f31430cc4152225ffc4eb112c
Camila21816 <capaucargu@uide.edu.ec>
2026-05-28 13:26:37 +0000

    install necessary package

--- security2.sh
+++ security2.sh
@@ -5,3 +5,15 @@ touch ~/test_group_heredado.txt
 ls -ls ~/test_group_heredado.txt 
 #the group is the user's primary group
 
+groups #View all available groups
+echo "Grupo actual: $(id -gn)" #see the current group
+#create a newgrp file
+touch ~/antes_de_newgrp.txt 
+ls -la ~/antes_de_newgrp.txt 
+
+#Install util-linux-extra
+apt update
+apt upgrade
+apt install -y apt-file && apt-file update
+apt-file search /usr/bin/newgrp
+apt install -y util-linux-extra 
