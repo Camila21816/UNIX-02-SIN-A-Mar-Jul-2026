41046d0d3642d455004a2d5bb0ea74dec25fa504
Camila21816 <capaucargu@uide.edu.ec>
2026-06-05 16:51:00 +0000

    add bash script demonstrating foreground and background sleep

---
+++ blackhatbash5.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+top #It displays a real-time list of all processes running on the system, sorted by CPU usage by default.
+top& #It tries to run the top monitor, but tells the system: "run this in the background and give me back control of the terminal immediately."
