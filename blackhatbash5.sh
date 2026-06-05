3ceeb9d54ea528744f81f0f8cb3ffd203f1f24df
Camila21816 <capaucargu@uide.edu.ec>
2026-06-05 17:16:48 +0000

    add script demonstrating stdout and stderr redirection

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
-top #It displays a real-time list of all processes running on the system, sorted by CPU usage by default.
-top& #It tries to run the top monitor, but tells the system: "run this in the background and give me back control of the terminal immediately."
+# top #It displays a real-time list of all processes running on the system, sorted by CPU usage by default.
+# top& #It tries to run the top monitor, but tells the system: "run this in the background and give me back control of the terminal immediately."
+
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "El comando lzl falló"
+
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Chao mundo" > output.txt
+ cat output.txt
+ echo "Chao chao  mundo" >> output.txt
+cat output.txt
+
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+lzl 2> error.txt
+cat error.txt
