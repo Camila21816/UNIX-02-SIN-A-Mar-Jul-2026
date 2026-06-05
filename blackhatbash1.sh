acfa8e3c329828666ce75132e9b9eeb8cb08879e
Camila21816 <capaucargu@uide.edu.ec>
2026-06-05 04:22:57 +0000

    fix command casing and add commented bash debugging reference

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+set -x
 bash --version
 env
 echo {SHELL}
 echo {RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+set +x
+# man ls (It is used to list the contents of a directory.)
+# ps -e -f
+# ps -ef
+# df
+# --human-readable
+# Put at the beginning of the script #!/usr/bin/env bash
+# bash -r blackhatbash1.sh (restricted debugging)
+# Debug script: bash -n blackhatbash1.sh
+# bash -x blackhatbash1.sh (debugs the entire script so it can generate a loop)
