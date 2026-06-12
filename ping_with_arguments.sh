d7c219e822091a65775d85bcda561554f1648080
Camila21816 <capaucargu@uide.edu.ec>
2026-06-12 03:36:26 +0000

    feat: implementar bucle de iteración de argumentos en el script

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -7,3 +7,11 @@ echo "Pinging the target: ${TARGET}..."
 ping -c 5 "${TARGET}"
 echo "The arguments are: $@" #Displays all the arguments or words you typed after the script name when you run it.
 echo "The total number of arguments is: $#" #Count how many arguments we passed to you in total, give the exact number.
+
+
+for args in "$@"; do
+echo "${args}"
+done
+#This new block is a loop that automatically iterates through all the parameters passed to the script when it runs. 
+#Instead of processing only the first target, the command checks the entire list of arguments one by one, temporarily,
+#stores the current element in an internal variable, and prints it to the screen on a separate line, repeating this cycle successively until all the entered words have been processed.
