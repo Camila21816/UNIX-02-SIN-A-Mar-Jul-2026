403cc37b1e476b7e21a84676a7eabafc54b338a6
Camila21816 <capaucargu@uide.edu.ec>
2026-06-12 03:52:42 +0000

    feat: create exit codes demonstration script

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -5,7 +5,7 @@ TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
 ping -c 5 "${TARGET}"
-echo "The arguments are: $@" #Displays all the arguments or words you typed after the script name when you run it.
+echo "The arguments are:" "$@" #Displays all the arguments or words you typed after the script name when you run it.
 echo "The total number of arguments is: $#" #Count how many arguments we passed to you in total, give the exact number.
 
 for args in "$@"; do
