62ced3e06d1efe372fdd01c5984d75286058f6c2
Camila21816 <capaucargu@uide.edu.ec>
2026-06-18 13:02:51 +0000

    Add awk examples for column filtering, NF usage, and custom field delimiters

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -8,3 +8,12 @@ ps | grep -i tty
 
 grep -v "35.237.4.214" log.txt
 grep -o "35.237.4.214" log.txt
+
+awk '{print $1}' log.txt
+awk '{print $1,$2,$3}' log.txt
+awk '{print $2}' log.txt
+awk '{print $3}' log.txt
+awk '{print $1,$NF}' log.txt
+touch example_csv.txt
+cat << 'EOF' > example_csv.txt
+awk -F',' '{print $1}' example_csv.txt
